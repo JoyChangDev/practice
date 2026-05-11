499a00802a04623cf0b4cb0d6152299f854c53b4
JoyChangDev <joychangdev@gmail.com>
2026-05-11 15:09:45 +0800

    feat(config): support SVG component imports; - Add Turbopack SVG handling with @svgr/webpack

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: "export",
   // Use production env for GitHub Pages base path.
-  basePath: process.env.NODE_ENV === 'production' ? '/practice' : '',
+  basePath: process.env.NODE_ENV === "production" ? "/practice" : "",
   experimental: {
-    optimizePackageImports: ['@chakra-ui/react'],
+    optimizePackageImports: ["@chakra-ui/react"],
+  },
+  // Supports importing SVG as React components @see {@link https://react-svgr.com/docs/next/}
+  turbopack: {
+    rules: {
+      "*.svg": {
+        loaders: ["@svgr/webpack"],
+        as: "*.js",
+      },
+    },
   },
 };
 
